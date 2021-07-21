daaf1b54c00c9ed316fcd2bf6b3b6c46935944bb
gabrielcoutinh0 <gabrielcoutinhof15@gmail.com>
2021-07-21 20:19:26 -0300

    Listando informações das pizzas.

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-pizzaJson.map((item, index) => {
-    let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true);
+const qSelector = (element) => document.querySelector(element);
+const qSelectorAll = (element) => document.querySelectorAll(element);
 
-    document.querySelector('.pizza-area').append(pizzaItem);
+pizzaJson.map((item, index) => {
+    let pizzaItem = qSelector('.models .pizza-item').cloneNode(true);
+
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+
+    qSelector('.pizza-area').append(pizzaItem);
 });
