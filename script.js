f4c7cbd865354051eaf1350994421a1eb1cb09a5
gabrielcoutinh0 <gabrielcoutinhof15@gmail.com>
2021-07-21 20:40:27 -0300

    Abrindo janela da pizza ao clicar.

--- script.js
+++ script.js
@@ -8,6 +8,14 @@ pizzaJson.map((item, index) => {
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    pizzaItem.querySelector('a').addEventListener('click', (e) => {
+        e.preventDefault();
+        qSelector('.pizzaWindowArea').style.opacity = 0;
+        qSelector('.pizzaWindowArea').style.display = 'flex';
+        setTimeout(() => {
+            qSelector('.pizzaWindowArea').style.opacity = 1;
+        }, 200);
+    });
 
     qSelector('.pizza-area').append(pizzaItem);
 });
